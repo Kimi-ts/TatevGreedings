058c45c67a98d2b5b369e8b3b0006b7a6812029a
Kimi-ts <r-g@tut.by>
2018-12-01 17:14:07 +0300

    split questions into two parts

--- js/app.js
+++ js/app.js
@@ -2,14 +2,81 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hi! This is a super-puper Christmas and Birthday quiz for you!',
-        showFinalText: false,
+        activeScreen: "welcomeScreen",
+        yesButtonDisabled: true,
+        noButtonDisabled: false,
         questions: [{
+            id: 0,
+            active: true,
+            text: "What is your profession?",
+            images: true,
+            answers: [{
+                    litera: "A",
+                    text: "QA",
+                    url: "",
+                    correct: true,
+                    id: 0
+                },
+                {
+                    litera: "B",
+                    text: "Developer",
+                    correct: false,
+                    id: 1
+                },
+                {
+                    litera: "C",
+                    text: "Manager",
+                    correct: false,
+                    id: 2
+                },
+                {
+                    litera: "D",
+                    text: "Director",
+                    correct: false,
+                    id: 3
+                }
+            ]
+        },
+        {
+            id: 1,
+            active: false,
+            text: "Where do you live?",
+            answers: [{
+                    litera: "A",
+                    text: "Armenia",
+                    correct: false,
+                    id: 0
+                },
+                {
+                    litera: "B",
+                    text: "Belarus",
+                    correct: true,
+                    id: 1
+                },
+                {
+                    litera: "C",
+                    text: "Canada",
+                    correct: false,
+                    id: 2
+                },
+                {
+                    litera: "D",
+                    text: "Laplandia",
+                    correct: false,
+                    id: 3
+                }
+            ]
+        }
+    ],
+        demoQuestions: [{
                 id: 0,
                 active: true,
                 text: "What is your profession?",
+                images: true,
                 answers: [{
                         litera: "A",
                         text: "QA",
+                        url: "",
                         correct: true,
                         id: 0
                     },
@@ -63,52 +130,50 @@ var app = new Vue({
                     }
                 ]
             }
-        ],
-        points: [{
-                id: 0,
-                value: 100,
-                active: true
-            },
-            {
-                id: 1,
-                value: 500,
-                active: false
-            },
-            {
-                id: 2,
-                value: "Final Prize!!!",
-                active: false
-            }
         ]
     },
     methods: {
         moveToNextQuestion: function (anwsSelectedId) {
-            console.log(currentQuestionId);
 
-            var currentQuestionId = this.questions.filter(function(item){
+            var questions;
+            if (this.activeScreen == 'demoQuiz'){
+                questions = this.demoQuestions;
+            }
+            else{
+                questions = this.questions;
+            }
+
+            var currentQuestionId = questions.filter(function(item){
                 return item.active == true;
             })[0].id;
 
-            if (this.questions[currentQuestionId].answers[anwsSelectedId].correct != true){
+            if (questions[currentQuestionId].answers[anwsSelectedId].correct != true){
                 alert("think again!")
                 return;
             }
 
-            if (currentQuestionId == this.questions.length) {
-                this.showFinalText = true;
-                this.points[currentQuestionId].active = false;
-                this.points[currentQuestionId + 1].active = true;
-            } else if(currentQuestionId < this.questions.length){
-                this.questions[currentQuestionId].active = false;
-                this.points[currentQuestionId].active = false;
-
-                this.points[currentQuestionId + 1].active = true;
-
-                if (currentQuestionId + 1 < this.questions.length){
-                    this.questions[currentQuestionId + 1].active = true;
+            if (currentQuestionId == questions.length) {
+                if (this.activeScreen == 'demoQuiz'){
+                    this.activeScreen = 'welcomeScreen';
+                    this.yesButtonDisabled = false;
                 }
                 else{
-                    this.showFinalText = true;
+                    this.activeScreen = 'finalText';
+                }
+            } else if(currentQuestionId < questions.length){
+                questions[currentQuestionId].active = false;
+
+                if (currentQuestionId + 1 < questions.length){
+                    questions[currentQuestionId + 1].active = true;
+                }
+                else{
+                    if (this.activeScreen == 'demoQuiz'){
+                        this.activeScreen = 'welcomeScreen';
+                        this.yesButtonDisabled = false;
+                    }
+                    else{
+                        this.activeScreen = 'finalText';
+                    }
                 }
             }
         }
